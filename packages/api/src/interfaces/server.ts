40d020ef67e6c70cc81273e4645f87fb0f2485b0
MiaoWoo <admin@yumc.pw>
2019-11-04 20:18:30 +0800

    feat: add server interface
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/api/src/interfaces/server.ts
+++ packages/api/src/interfaces/server.ts
@@ -1,4 +1,9 @@
 export namespace server {
     export const ServerType = Symbol("ServerType");
     export const Console = Symbol("Console");
+    export const Server = Symbol("Server");
+    export interface Server {
+        getVersion(): string;
+        getPlayer(name: string): any;
+    }
 }
