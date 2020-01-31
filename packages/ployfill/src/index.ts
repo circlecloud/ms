8c4f2663561ff9339f9608e331a171b84bc55b60
MiaoWoo <admin@yumc.pw>
2020-02-01 02:44:23 +0800

    feat: move global declare to @ms/ployfill
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/ployfill/src/index.ts
+++ packages/ployfill/src/index.ts
@@ -1,7 +1,17 @@
-import { XMLHttpRequest as xhr } from './xml-http-request'
+import './global'
+import '@ms/nashorn'
+import { XMLHttpRequest } from './xml-http-request'
 
-var XMLHttpRequest = xhr;
-
-export {
-    XMLHttpRequest
-}
+let ployfillStartTime = new Date().getTime();
+console.info('Initialization Java Nashorn ployfill. Please wait...');
+// String contains ployfill
+Object.defineProperty(String.prototype, 'contains', {
+    value: function(searchString: string, position: number) {
+        return String.prototype.indexOf.call(this, searchString, position) > -1;
+    }
+})
+// ES6 Map Symbol ployfill
+require('es6-map/implement');
+require('es6-symbol/implement');
+global.setGlobal('XMLHttpRequest', XMLHttpRequest)
+console.info('Java Nashorn ployfill loading completed... Cost (' + (new Date().getTime() - ployfillStartTime) / 1000 + 's)!');
