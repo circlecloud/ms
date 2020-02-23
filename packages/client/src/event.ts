11c91600da0c48b0ea9cd5a31997f0df91272937
MiaoWoo <admin@yumc.pw>
2020-02-23 22:35:05 +0800

    feat: add auto respawn support
    Signed-off-by: MiaoWoo <admin@yumc.pw>

---
+++ packages/client/src/event.ts
@@ -0,0 +1,27 @@
+import { $ } from './color'
+
+export function attachEvents(client) {
+    client.on('chat', (packet) => {
+        // Listen for chat messages and echo them back.
+        var jsonMsg = JSON.parse(packet.message);
+        console.log($(jsonMsg))
+    })
+    client.on('state', (newState, oldState) => {
+        console.log('Client Change State', oldState, 'to', newState)
+        let targetServer = process.argv[3]
+        if (newState == "play" && targetServer) {
+            setTimeout(() => {
+                client.write('chat', {
+                    message: '/server ' + targetServer
+                })
+            }, 3000)
+        }
+    })
+    client.on('update_health', (packet) => {
+        if (packet.health <= 0) {
+            console.log("Player Dead Auto Respawn...")
+            client.write('client_command', { payload: 0 })
+        } else if (packet.health > 0) {
+        }
+    })
+}
