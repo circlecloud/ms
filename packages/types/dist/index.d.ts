e9ac46487c2c0388cd50e06db94e356d0c77b6c1
MiaoWoo <admin@yumc.pw>
2020-03-02 22:29:29 +0800

    feat: merge type defiend
    Signed-off-by: MiaoWoo <admin@yumc.pw>

---
+++ packages/types/dist/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./typings/bukkit/index.d.ts" />
+/// <reference path="./typings/bungee/index.d.ts" />
+/// <reference path="./typings/nukkit/index.d.ts" />
+/// <reference path="./typings/sponge/index.d.ts" />
