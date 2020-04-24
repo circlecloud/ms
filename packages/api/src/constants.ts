acbeda965946a8bffcd023ba4b190f06c9e5fd1f
MiaoWoo <admin@yumc.pw>
2020-04-24 13:04:45 +0800

    feat: support base64 source map
    Signed-off-by: MiaoWoo <admin@yumc.pw>

---
+++ packages/api/src/constants.ts
@@ -0,0 +1,16 @@
+export namespace constants {
+    export namespace Reflect {
+        export const Method = {
+            getServerConnection: [/*spigot 1.8.8*/'aq',/*spigot 1.12.2*/ 'an', /*spigot 1.14.4+*/'getServerConnection', /*catserver 1.12.2*/'func_147137_ag']
+        }
+        export const Field = {
+            listeningChannels: [/*spigot 1.8.8-1.12.2*/'g', /*spigot 1.14.4*/'f', /*spigot 1.15.2+*/'listeningChannels', /*catserver 1.12.2*/'field_151274_e']
+        }
+    }
+    export enum ServerType {
+        Bukkit = 'bukkit',
+        Sponge = 'sponge',
+        Nukkit = 'nukkit',
+        Bungee = 'bungee'
+    }
+}
