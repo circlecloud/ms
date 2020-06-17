f573d0c7589cde1fe07bbcdd73f5d804879ba434
MiaoWoo <admin@yumc.pw>
2020-06-17 18:39:24 +0800

    feat: support loader & scanner
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,3 +1,15 @@
+import './scanner/file-scanner'
+import './loader/ioc-loader'
+import './loader/basic-loader'
+
 export * from './manager'
 export * from './decorators'
 export * from './interfaces'
+
+export {
+    plugin as JSPlugin,
+    cmd as Cmd,
+    tab as Tab,
+    listener as Listener,
+    config as Config
+} from './decorators'
