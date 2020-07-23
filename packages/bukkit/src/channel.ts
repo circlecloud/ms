20749d33815ae9ccf7adb319bea88d446d52416e
MiaoWoo <admin@yumc.pw>
2020-07-23 13:44:39 +0800

    feat: add qrcode lib
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/bukkit/src/channel.ts
+++ packages/bukkit/src/channel.ts
@@ -8,18 +8,30 @@ const Messenger = Bukkit.getMessenger()
 @provideSingleton(channel.Channel)
 export class BukkitChannel extends channel.Channel {
     @inject(plugin.PluginInstance)
-    private pluginInstance: any;
+    private pluginInstance: any
 
-    send(player: any, channel: string, data: any) {
-        player.sendPluginMessage(this.pluginInstance, channel, data);
+    /**
+     * 给玩家发送通道消息
+     * @param player 接受消息的玩家
+     * @param channel 通道
+     * @param data 数据
+     */
+    send(player: any, channel: string, data: number[]) {
+        player.sendPluginMessage(this.pluginInstance, channel, data)
     }
+
+    /**
+     * 注册通道监听器
+     * @param channel 通道
+     * @param listener 监听器
+     */
     register(channel: string, listener: channel.ChannelListener) {
         Messenger.registerIncomingPluginChannel(this.pluginInstance, channel, new PluginMessageListener({
             onPluginMessageReceived: (/**String */ channel, /**Player */ player, /**byte[] */data) => {
                 listener(data, { channel, player, data })
             }
-        }));
-        Messenger.registerOutgoingPluginChannel(this.pluginInstance, channel);
+        }))
+        Messenger.registerOutgoingPluginChannel(this.pluginInstance, channel)
     }
     unregister(channel: string, listener: any) {
         Messenger.unregisterIncomingPluginChannel(this.pluginInstance, channel)
