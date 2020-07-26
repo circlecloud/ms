8def6257df0ed36a51a0073e92b3c8cd01f57a49
MiaoWoo <admin@yumc.pw>
2020-07-26 21:33:58 +0800

    fix: readSourceMap split error
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/common/src/qrcode.ts
+++ packages/common/src/qrcode.ts
@@ -504,22 +504,22 @@ export enum QRErrorCorrectLevel {
 // QRMaskPattern
 //---------------------------------------------------------------------
 
-var QRMaskPattern = {
-    PATTERN000: 0,
-    PATTERN001: 1,
-    PATTERN010: 2,
-    PATTERN011: 3,
-    PATTERN100: 4,
-    PATTERN101: 5,
-    PATTERN110: 6,
-    PATTERN111: 7
+export enum QRMaskPattern {
+    PATTERN000 = 0,
+    PATTERN001 = 1,
+    PATTERN010 = 2,
+    PATTERN011 = 3,
+    PATTERN100 = 4,
+    PATTERN101 = 5,
+    PATTERN110 = 6,
+    PATTERN111 = 7
 }
 
 //---------------------------------------------------------------------
 // QRUtil
 //---------------------------------------------------------------------
 
-var QRUtil = {
+export const QRUtil = {
 
     PATTERN_POSITION_TABLE: [
         [],
