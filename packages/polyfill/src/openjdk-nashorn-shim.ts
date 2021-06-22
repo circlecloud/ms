88e59231da50b9623d775765fbdff31fa32d83e5
MiaoWoo <admin@yumc.pw>
2021-06-22 15:51:28 +0800

    feat: add openjdk nashorn shim
    Signed-off-by: MiaoWoo <admin@yumc.pw>

---
+++ packages/polyfill/src/openjdk-nashorn-shim.ts
@@ -0,0 +1,7 @@
+// fix OpenJDK Nahsorn setPrototypeOf Bug
+try {
+    Java.type('org.openjdk.nashorn.api.scripting.NashornScriptEngine')
+    Object.setPrototypeOf = ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b })
+} catch (error) {
+}
+export { }
