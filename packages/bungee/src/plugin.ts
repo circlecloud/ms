7afea75454e48179cd0c8201011808b4426f7807
MiaoWoo <admin@yumc.pw>
2021-08-03 17:56:11 +0800

    feat: 更新插件管理模块
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/bungee/src/plugin.ts
+++ packages/bungee/src/plugin.ts
@@ -3,10 +3,34 @@ import { server } from '@ccms/api'
 let Bungee: net.md_5.bungee.api.ProxyServer = base.getInstance().getProxy()
 
 export class BungeeNativePluginManager extends server.NativePluginManager {
-    has(name: string) {
-        return !!this.get(name)
+    private bungeePluginManager: net.md_5.bungee.api.plugin.PluginManager
+
+    constructor() {
+        super()
+        this.bungeePluginManager = Bungee.getPluginManager()
     }
-    get(name: string) {
-        return Bungee.getPluginManager().getPlugin(name)
+
+    list(): server.NativePlugin[] {
+        return Java.from(this.bungeePluginManager.getPlugins()).map(plugin => this.convert(plugin))
+    }
+    has(name: string): boolean {
+        return !!this.bungeePluginManager.getPlugin(name)
+    }
+    get(name: string): server.NativePlugin {
+        return this.convert(this.bungeePluginManager.getPlugin(name))
+    }
+
+    private convert(plugin: net.md_5.bungee.api.plugin.Plugin): server.NativePlugin {
+        if (!plugin) return plugin as any
+        let desc = plugin.getDescription()
+        return {
+            name: desc.getName(),
+            version: desc.getVersion(),
+            authors: [desc.getAuthor()],
+            depends: Java.from(desc.getDepends()) as any,
+            softDepends: Java.from(desc.getSoftDepends()) as any,
+            enable: true,
+            origin: plugin
+        }
     }
 }
--- packages/nukkit/src/plugin.ts
+++ packages/nukkit/src/plugin.ts
@@ -3,10 +3,34 @@ import { server } from '@ccms/api'
 let Nukkit: cn.nukkit.Server = base.getInstance().getServer()
 
 export class NukkitNativePluginManager extends server.NativePluginManager {
+    private nukkitPluginManager: cn.nukkit.plugin.PluginManager
+
+    constructor() {
+        super()
+        this.nukkitPluginManager = Nukkit.getPluginManager()
+    }
+
+    list(): server.NativePlugin[] {
+        return Java.from(this.nukkitPluginManager.getPlugins().values()).map(plugin => this.convert(plugin))
+    }
     has(name: string) {
-        return !!this.get(name)
+        return !!this.nukkitPluginManager.getPlugin(name)
     }
     get(name: string) {
-        return Nukkit.getPluginManager().getPlugin(name)
+        return this.convert(this.nukkitPluginManager.getPlugin(name))
+    }
+
+    private convert(plugin: cn.nukkit.plugin.Plugin): server.NativePlugin {
+        if (!plugin) return plugin as any
+        let desc = plugin.getDescription()
+        return {
+            name: plugin.getName(),
+            version: desc.getVersion(),
+            authors: Java.from(desc.getAuthors() as string[]),
+            depends: Java.from(desc.getDepend() as string[]),
+            softDepends: Java.from(desc.getSoftDepend() as string[]),
+            enable: plugin.isEnabled(),
+            origin: plugin
+        }
     }
 }
