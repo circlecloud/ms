2aef84604b97e319ad4ddfee85e75058099349a5
MiaoWoo <admin@yumc.pw>
2022-04-03 03:07:18 +0800

    fix: api interface error
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/core/src/exit.js
+++ packages/core/src/exit.js
@@ -1,4 +1,14 @@
+function upgradeModules(core) {
+    if (base.version && global.ScriptEngineVersion != core['dist-tags']['latest']) {
+        var Paths = Java.type('java.nio.file.Paths')
+        base.save(Paths.get(root, "upgrade"), core['dist-tags']['latest'])
+        console.info('@ccms/core found new version ' + core['dist-tags']['latest'] + ' will upgrade after reboot!')
+    }
+}
 function exit() {
+    var http = require('@ccms/common/dist/http').default
+    upgradeModules(http.get('https://registry.npmmirror.com/@ccms/core'))
+
     console.debug('exit finish!')
 }
 exit()
