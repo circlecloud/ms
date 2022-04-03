16502ecc012a392b1d22fdbfe583f0690b6408c3
MiaoWoo <admin@yumc.pw>
2022-04-04 05:13:24 +0800

    feat: remove global initialize
    Signed-off-by: MiaoWoo <admin@yumc.pw>

--- packages/core/src/exit.js
+++ packages/core/src/exit.js
@@ -1,14 +1,13 @@
-function upgradeModules(core) {
-    if (base.version && global.ScriptEngineVersion != core['dist-tags']['latest']) {
-        var Paths = Java.type('java.nio.file.Paths')
-        base.save(Paths.get(root, "upgrade"), core['dist-tags']['latest'])
-        console.info('@ccms/core found new version ' + core['dist-tags']['latest'] + ' will upgrade after reboot!')
-    }
-}
 function exit() {
     var http = require('@ccms/common/dist/http').default
+    function upgradeModules(core) {
+        if (base.version && global.ScriptEngineVersion != core['dist-tags']['latest']) {
+            var Paths = Java.type('java.nio.file.Paths')
+            base.save(Paths.get(root, "upgrade"), core['dist-tags']['latest'])
+            console.info('@ccms/core found new version ' + core['dist-tags']['latest'] + ' will upgrade after reboot!')
+        }
+    }
     upgradeModules(http.get('https://registry.npmmirror.com/@ccms/core'))
-
     console.debug('exit finish!')
 }
 exit()
