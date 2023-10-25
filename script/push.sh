2b7588cf0459f6c1852db461a85b53561536470d
MiaoWoo <admin@yumc.pw>
2023-10-25 14:39:09 +0800

    chore: change lerna to lerna-lite

--- script/push.sh
+++ script/push.sh
@@ -3,6 +3,6 @@ DISTTAG=${1:latest}
 for package in `ls packages`; do
     echo $package
     pushd packages/$package
-    npm publish --tag ${DISTTAG} --access=public --registry https://registry.npmjs.org
+    pnpm publish --tag ${DISTTAG} --access=public --registry https://registry.npmjs.org
     popd
 done
